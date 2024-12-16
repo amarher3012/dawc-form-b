f35489d77d805a4388c194ab438b6ea887196140
Alejandro Martin Herrera <amarher3012@fp.iesromerovargas.com>
2024-12-16 18:14:32 +0100

    Fix issues and reformat with tabs

--- assets/app.js
+++ assets/app.js
@@ -1,37 +1,52 @@
 const provinceSelect = document.getElementById('provinces');
-const citySelect = document.getElementById('city');
+const citySelect = document.getElementById('cities');
 
 const provinces = {
-    almeria: ['Almería', 'Roquetas de Mar', 'El Ejido', 'Níjar', 'Vera'],
-    cadiz: [
-        'Cádiz',
-        'Jerez de la Frontera',
-        'Algeciras',
-        'San Fernando',
-        'Chiclana de la Frontera',
-    ],
-    cordoba: ['Córdoba', 'Lucena', 'Puente Genil', 'Montilla', 'Pozoblanco'],
-    granada: ['Granada', 'Motril', 'Almuñécar', 'Baza', 'Maracena'],
-    huelva: ['Huelva', 'Lepe', 'Ayamonte', 'Almonte', 'Valverde del Camino'],
-    jaen: ['Jaén', 'Andújar', 'Úbeda', 'Linares', 'Baeza'],
-    malaga: ['Málaga', 'Marbella', 'Ronda', 'Torremolinos', 'Fuengirola'],
-    sevilla: [
-        'Sevilla',
-        'Alcalá de Guadaíra',
-        'Dos Hermanas',
-        'Coria del Río',
-        'Mairena del Aljarafe',
-    ],
+	Almería: ['Almería', 'Roquetas de Mar', 'El Ejido', 'Níjar', 'Vera'],
+	Cádiz: [
+		'Cádiz',
+		'Jerez de la Frontera',
+		'Algeciras',
+		'San Fernando',
+		'Chiclana de la Frontera',
+	],
+	Córdoba: ['Córdoba', 'Lucena', 'Puente Genil', 'Montilla', 'Pozoblanco'],
+	Granada: ['Granada', 'Motril', 'Almuñécar', 'Baza', 'Maracena'],
+	Huelva: ['Huelva', 'Lepe', 'Ayamonte', 'Almonte', 'Valverde del Camino'],
+	Jaén: ['Jaén', 'Andújar', 'Úbeda', 'Linares', 'Baeza'],
+	Málaga: ['Málaga', 'Marbella', 'Ronda', 'Torremolinos', 'Fuengirola'],
+	Sevilla: [
+		'Sevilla',
+		'Alcalá de Guadaíra',
+		'Dos Hermanas',
+		'Coria del Río',
+		'Mairena del Aljarafe',
+	],
+	// Add more provinces here
 };
 
-provinceSelect.addEventListener('input', () => {
-    const cities = [...citySelect];
-    cities.forEach((city) => city.remove());
+// Renders provinces
+Object.keys(provinces).forEach((province) => {
+	const provinceList = document.createElement('option');
+	provinceList.value = province;
+	provinceList.textContent = province;
+	provinceSelect.appendChild(provinceList);
+});
 
-    provinces[provinceSelect.value].forEach((city) => {
-        const cityList = document.createElement('option');
-        cityList.value = city;
-        cityList.textContent = city;
-        citySelect.appendChild(cityList);
-    });
+// Renders cities depending on province selection
+provinceSelect.addEventListener('input', () => {
+	const cities = [...citySelect];
+	cities.forEach((city) => {
+		// Optional check to keep the default value in
+		if (city.value !== 'default') {
+			city.remove();
+		}
+	});
+
+	provinces[provinceSelect.value].forEach((city) => {
+		const cityList = document.createElement('option');
+		cityList.value = city;
+		cityList.textContent = city;
+		citySelect.appendChild(cityList);
+	});
 });
